1280f76cff72286d2d0edf3a118a928267dff4ec
white-panda01 <hars0101@gmail.com>
2020-10-23 15:41:32 +0400

    port scanner multi-format support added

--- port-scanner.ps1
+++ port-scanner.ps1
@@ -1,15 +1,20 @@
 ﻿function Scan-Port
-{$computer=Read-Host "Computername | IP Address?"
- $port=Read-Host "Port Numbers? Separate them by comma"
+{$computer= Read-Host "enter IP"
+ $port=Read-Host "enter ports"
  $port.split(',') | Foreach-Object -Process {If (($a=Test-NetConnection $computer -Port $_ -WarningAction SilentlyContinue).tcpTestSucceeded -eq $true) {Write-Host $a.Computername $a.RemotePort -ForegroundColor Green -Separator " ==> "} else {Write-Host $a.Computername $a.RemotePort -Separator " ==> " -ForegroundColor Red}}
  }
 
-function Choose($choice){
-    If($choice -eq '1'){
-        Scan-Port
-    }else{
-        Write-Host 'Bruh'
+
+ function new-scan-port{
+    $enterPorts = Read-Host "enter ports in format 21-80"
+    $p1, $p2 = $enterPorts.Split('-')
+    $ports = $p1..$p2
+    foreach ($port in $ports){
+       if(($a=Test-NetConnection '192.168.1.109' -Port $port -WarningAction SilentlyContinue).tcpTestSucceeded -eq $true){
+            Write-Host $a.Computername -Separator "==>" $a.RemotePort -ForegroundColor Green
+       }else {
+            Write-Host $a.Computername -Separator "==>" $a.RemotePort -ForegroundColor Red
+       }
     }
 }
-
-Choose(1)
+new-scan-port
