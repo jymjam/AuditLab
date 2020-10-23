3e1364530bb2a9dcda04e25b83752593644cc02d
white-panda01 <hars0101@gmail.com>
2020-10-23 18:13:23 +0400

    refactored code for single ip scan

--- port-scanner.ps1
+++ port-scanner.ps1
@@ -1,20 +1,56 @@
-﻿function Scan-Port
-{$computer= Read-Host "enter IP"
- $port=Read-Host "enter ports"
+﻿function Scan-ort($ip)
+{$computer=$ip
+ $port=Read-Host "enter ports eg: 22,25,80"
  $port.split(',') | Foreach-Object -Process {If (($a=Test-NetConnection $computer -Port $_ -WarningAction SilentlyContinue).tcpTestSucceeded -eq $true) {Write-Host $a.Computername $a.RemotePort -ForegroundColor Green -Separator " ==> "} else {Write-Host $a.Computername $a.RemotePort -Separator " ==> " -ForegroundColor Red}}
  }
 
-
- function new-scan-port{
-    $enterPorts = Read-Host "enter ports in format 21-80"
+function ne-port($ip){
+    $enterPorts = Read-Host "enter port range. eg: 21-80"
     $p1, $p2 = $enterPorts.Split('-')
     $ports = $p1..$p2
     foreach ($port in $ports){
-       if(($a=Test-NetConnection '192.168.1.109' -Port $port -WarningAction SilentlyContinue).tcpTestSucceeded -eq $true){
+       if(($a=Test-NetConnection $ip -Port $port -WarningAction SilentlyContinue).tcpTestSucceeded -eq $true){
             Write-Host $a.Computername -Separator "==>" $a.RemotePort -ForegroundColor Green
        }else {
             Write-Host $a.Computername -Separator "==>" $a.RemotePort -ForegroundColor Red
        }
     }
 }
-new-scan-port
+###################################3
+function new-scan-port($ip){
+    $port_format = Read-Host '1 > enter port range eg: 21-80 || 2> enter individual ports eg: 22,53,80,443'
+
+    if($port_format -eq '1'){
+
+        $enterPorts = Read-Host "enter port range. eg: 21-80"
+        $p1, $p2 = $enterPorts.Split('-')
+        $ports = $p1..$p2
+
+        foreach ($port in $ports){
+           if(($a=Test-NetConnection $ip -Port $port -WarningAction SilentlyContinue).tcpTestSucceeded -eq $true){
+                Write-Host $a.Computername -Separator "==>" $a.RemotePort -ForegroundColor Green
+           }else {
+                Write-Host $a.Computername -Separator "==>" $a.RemotePort -ForegroundColor Red
+           }
+        }
+    }else{
+        $port=Read-Host "enter ports eg: 22,25,80"
+        $port.split(',') | Foreach-Object -Process {If (($a=Test-NetConnection $ip -Port $_ -WarningAction SilentlyContinue).tcpTestSucceeded -eq $true) {Write-Host $a.Computername $a.RemotePort -ForegroundColor Green -Separator " ==> "} else {Write-Host $a.Computername $a.RemotePort -Separator " ==> " -ForegroundColor Red}}
+    }
+
+    
+}
+
+function main(){
+    $format = Read-Host "enter 1 > IP address range (eg: 10.10.10.0-10.10.10.255) || 2 > IP address"
+    if($format -eq '1'){
+        $ip_range = Read-Host 'Enter IP range'
+    }elseif ($format -eq '2'){
+        $ip = Read-Host 'Enter an IP add'
+        new-scan-port($ip)
+    }else{
+        Write-Host 'invalid choice'
+    }
+
+}
+main
