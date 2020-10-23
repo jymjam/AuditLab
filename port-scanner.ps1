5e4b4d3517d3a760813715b9cb450357ad03a4eb
white-panda01 <hars0101@gmail.com>
2020-10-23 14:10:55 +0400

    port-scanner prototype w/ powershell

---
+++ port-scanner.ps1
@@ -0,0 +1,15 @@
+﻿function Scan-Port
+{$computer=Read-Host "Computername | IP Address?"
+ $port=Read-Host "Port Numbers? Separate them by comma"
+ $port.split(',') | Foreach-Object -Process {If (($a=Test-NetConnection $computer -Port $_ -WarningAction SilentlyContinue).tcpTestSucceeded -eq $true) {Write-Host $a.Computername $a.RemotePort -ForegroundColor Green -Separator " ==> "} else {Write-Host $a.Computername $a.RemotePort -Separator " ==> " -ForegroundColor Red}}
+ }
+
+function Choose($choice){
+    If($choice -eq '1'){
+        Scan-Port
+    }else{
+        Write-Host 'Bruh'
+    }
+}
+
+Choose(1)
